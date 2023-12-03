957debdff6b605078a6e7d11d193b37a9dd01377
roopeak <roopeak@protonmail.com>
2023-12-03 17:42:38 +0200

    3.15 step3

--- index.js
+++ index.js
@@ -59,24 +59,26 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) 
-  {
-    res.json(person)
-  } 
-  else 
-  {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
+
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
 })
 
 const generateId = () => {
