98bf41c84ebce04d6113cd0dfd4f3bc60c3fdbf5
roopeak <roopeak@protonmail.com>
2023-12-03 19:14:11 +0200

    3.16 step4

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const cors = require('cors')
 const Person = require('./models/person')
 const app = express()
 
-
-
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -25,40 +23,18 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
 )
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendic",
-    number: "39-23-6423122"
-  }
-]
-
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(persons => {
       res.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
@@ -67,25 +43,22 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      res.status(400).send({ error: 'malformatted id' })
-    })
-
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then((result) => {
       res.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
   return Math.floor(Math.random() * 100)
 }
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   const person = new Person({
@@ -94,10 +67,30 @@ app.post('/api/persons', (req, res) => {
     number: body.number,
   })
 
+  if (body.name.length == 0 || body.number.length == 0)
+  {
+    return res.status(400).json({
+      error: 'name or number is blank'
+    })
+  }
+
   person.save().then(savedPerson => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
