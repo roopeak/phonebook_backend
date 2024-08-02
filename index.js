d2a1887e3bfcc4502dbc66d7f8685f8db2d48900
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-02 14:14:11 +0300

    3.8 step8

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
 
+morgan.token('data', (request, response) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  }
+
+  return ' '
+})
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
 
 let persons = [
   {
@@ -91,7 +101,6 @@ app.post('/api/persons', (request, response) => {
   persons = persons.concat(person)
 
   response.json(person)
-
 })
 
 const PORT = 3001
