a5bc38dae0fa4b8b8244e483b645b948b579f352
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-05 09:56:07 +0300

    3.10 step10

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 const cors = require('cors')
+
+const app = express()
 
 app.use(express.json())
 app.use(cors())
