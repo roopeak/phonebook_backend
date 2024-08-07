aae24ced0daaa8cc597d3a280078bfef84e2adc3
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-07 14:09:35 +0300

    3.16 step4

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const Person = require('./models/person')
 
 const app = express()
 
-app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
 morgan.token('data', (request, response) => {
   if (request.method === 'POST') {
@@ -17,6 +18,7 @@ morgan.token('data', (request, response) => {
 
   return ' '
 })
+
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data')
@@ -35,22 +37,23 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -59,17 +62,38 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-
+  
   const person = new Person({
     id: generateId(),
     name: body.name,
     number: body.number
   })
-
-  person.save().then(savedPerson => {
+  
+  person
+  .save()
+  .then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
