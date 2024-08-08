27a2ee66443dd24348501c81c70de8a767e71683
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-08 10:21:21 +0300

    3.19 step7

--- index.js
+++ index.js
@@ -86,12 +86,11 @@ app.post('/api/persons', (request, response, next) => {
     number: body.number
   })
   
-  person
-  .save()
-  .then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -105,6 +104,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
