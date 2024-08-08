b05641d84aaa52132865fd26a6805ded2cfde5b4
Roope Kärkkäinen <roopeak@protonmail.com>
2024-08-08 10:45:02 +0300

    3.20 step8

--- models/person.js
+++ models/person.js
@@ -18,7 +18,16 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3
   },
-  number: String
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{7}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
